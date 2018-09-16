dc6f7e7534986e024ecdf01a748e65b6bb21933b
pchome <ldj@qq.com>
2018-09-16 21:10:30 +0800

    Updates README

--- swoole/demo/www/client/index.php
+++ swoole/demo/www/client/index.php
@@ -5,6 +5,6 @@ if (!$client->connect('swoole-server', 9501, -1))
 {
     exit("connect failed. Error: {$client->errCode}\n");
 }
-$client->send("hello world\n");
+$client->send("Hello Swoole!\n");
 echo $client->recv();
 $client->close();
